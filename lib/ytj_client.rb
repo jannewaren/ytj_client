2b66aab75b55ddbe17b99c0b497af701e143a48b
Janne Warén <janne.waren@visma.com>
2016-12-27 21:44:34 +0200

    active_support/core_ext/string/inflections

--- lib/ytj_client.rb
+++ lib/ytj_client.rb
@@ -6,7 +6,7 @@ require 'active_support'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/hash/slice'
 require 'active_support/core_ext/hash/keys'
-
+require 'active_support/core_ext/string/inflections'
 
 module YtjClient
 
